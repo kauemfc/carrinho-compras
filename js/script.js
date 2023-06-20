c121da2edfb2860108dc9178a4862047ca60f334
Kaue Camargo <kaue.mcamargo@gmail.com>
2023-06-20 09:10:21 -0300

    Teste

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+//Listagem dos modelos
+modeloJson.map((item, index) => {
+    let modeloItem = document.querySelector('.models .modelo-item').cloneNode(true);
+
+    modeloItem.setAttribute('data-key', index);
+
+    modeloItem.querySelector('.modelo-item-img img').src = item.img;
+    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
+    modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    modeloItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let key = e.target.closest('.modelo-item').getAttribute('data-key');
+        console.log('modelo clicado: ' + key);
+
+        c('.modeloBig img').src = modeloJson[key].img;
+        c('.modeloInfo h1').innerHTML = modeloJson[key].name;
+        c('.modeloInfo-desc').innerHTML = modeloJson[key].description;
+        c('.modeloInfo-actualPrice').innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`
+
+        cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+            size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeIndex];
+        });
+
+        c('.modeloWindowArea').style.opacity = 0
+        c('.modeloWindowArea').style.display = 'flex'
+
+        setTimeout(() => {c('.modeloWindowArea').style.opacity = 1
+    }, 200)
+    })
+
+    c('.modelo-area').append(modeloItem);
+}) 
